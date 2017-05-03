473bd3259b9521a504d3ececb39005f2b5d58b4d
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2017-05-03 15:29:22 +0200

    Move HttpWrapper to MovieDetails Component

--- lib/components/movie-details.jsx
+++ lib/components/movie-details.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import HttpWrapper from "../modules/http-wrapper";
 
 class MovieDetails extends React.Component {
 	constructor(props) {
@@ -6,9 +7,14 @@ class MovieDetails extends React.Component {
 	}
 	componentDidMount() {
 		console.log(this.props.match.params.movie_id);
+		HttpWrapper.getMovieDetails(
+			this.props.match.params.movie_id
+		).then(response => {
+			console.log(response.data);
+		});
 	}
 	componentWillReceiveProps(nextProps) {
-		console.log(nextProps.match.params.movie_id);
+		// console.log(nextProps.match.params.movie_id);
 	}
 	render() {
 		return <div>MovieDetails: {this.props.match.params.movie_id}</div>;
