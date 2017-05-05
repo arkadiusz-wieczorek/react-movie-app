2b0364e7ce748abd0e1866f1f229c49d882c6727
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2017-05-05 02:35:29 +0200

    Fix render function in MovieDetails Component

--- lib/components/movie-details.jsx
+++ lib/components/movie-details.jsx
@@ -8,16 +8,20 @@ class MovieDetails extends React.Component {
 		return (
 			<div>
 				<ul>
-					<li>{this.props.poster_path}</li>
-					<li>{this.props.id}</li>
-					<li>{this.props.title}</li>
-					<li>{this.props.release_date}</li>
-					<li>{this.props.vote_count}</li>
-					<li>{this.props.vote_average}</li>
-					<li>{this.props.overview}</li>
-					<li>{this.props.genres}</li>
-					<li>{this.props.production_countries}</li>
-					<li>{this.props.production_countries}</li>
+					<li>{this.props.movie.poster_path}</li>
+					<li>{this.props.movie.id}</li>
+					<li>{this.props.movie.title}</li>
+					<li>{this.props.movie.release_date}</li>
+					<li>{this.props.movie.vote_count}</li>
+					<li>{this.props.movie.vote_average}</li>
+					<li>{this.props.movie.overview}</li>
+					<li>{JSON.stringify(this.props.movie.genres)}</li>
+					<li>
+						{JSON.stringify(this.props.movie.production_countries)}
+					</li>
+					<li>
+						{JSON.stringify(this.props.movie.production_companies)}
+					</li>
 
 				</ul>
 
