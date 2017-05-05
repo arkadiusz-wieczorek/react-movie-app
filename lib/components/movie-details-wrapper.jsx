f5b104bd4e29cd8f5770e41d7a7f73482658a741
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2017-05-05 18:15:01 +0200

    Change MovieDetailsWrapper Component to HigherOrder Component

--- lib/components/movie-details-wrapper.jsx
+++ lib/components/movie-details-wrapper.jsx
@@ -1,67 +1,91 @@
 import React, { Component } from "react";
 import HttpWrapper from "../modules/http-wrapper";
-import MovieDetails from "./movie-details.jsx";
+// import MovieDetails from "./movie-details.jsx";
 
-class MovieDetailsWrapper extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			poster_path: "",
-			id: this.props.match.params.movie_id,
-			title: "",
-			release_date: "",
-			vote_count: null,
-			vote_average: null,
-			overview: "",
+export default function(MovieDisplayComponent) {
+	return class MovieDetailsWrapper extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = {
+				poster_path: "",
+				// id: this.getMovieId(),
+				title: "",
+				release_date: "",
+				vote_count: null,
+				vote_average: null,
+				overview: "",
 
-			genres: [],
-			production_countries: [],
-			production_companies: [],
-		};
-	}
+				genres: [],
+				production_countries: [],
+				production_companies: [],
 
-	getMovie() {
-		for (var m in this.props.movies) {
-			if (this.props.movies[m].id == this.state.id)
-				return this.props.movies[m];
-		}
-		return null;
-	}
-
-	mergeObjectWithState(obj) {
-		let movie_state = this.state;
-
-		Object.keys(obj)
-			.filter(key => key in movie_state)
-			.forEach(key => (movie_state[key] = obj[key]));
-
-		return movie_state;
-	}
-	componentDidMount() {
-		let merged_object, movie = this.getMovie();
-		if (movie !== null) {
-			merged_object = this.mergeObjectWithState(movie);
-			this.setState({ merged_object });
+				loaded: false,
+			};
 		}
 
-		HttpWrapper.getMovieDetails(this.state.id).then(response => {
-			merged_object = this.mergeObjectWithState(response.data);
-			this.setState({ merged_object });
-		});
-	}
+		getMovieId() {
+			return this.props.match.params.movie_id;
+		}
 
-	shouldComponentUpdate(nextProps, nextState) {
-		if (nextState !== this.state) return true;
-		else return false;
-	}
+		selectMovie() {
+			for (var m in this.props.movies) {
+				if (this.props.movies[m].id == this.getMovieId())
+					return this.props.movies[m];
+			}
+			return null;
+		}
 
-	render() {
-		return (
-			<div>
-				MoviesDetailsWrapper
-				<MovieDetails movie={this.state} />
-			</div>
-		);
-	}
+		getMovie(id) {
+			this.setState({ loaded: false });
+			let merged_object, movie = this.selectMovie();
+			if (movie !== null) {
+				merged_object = this.mergeObjectWithState(movie);
+				this.setState(merged_object);
+			}
+			HttpWrapper.getMovieDetails(id).then(response => {
+				merged_object = this.mergeObjectWithState(response.data);
+				merged_object.loaded = true;
+				console.log(this.state);
+				this.setState(merged_object);
+			});
+		}
+
+		mergeObjectWithState(obj) {
+			let movie_state = this.state;
+
+			Object.keys(obj)
+				.filter(key => key in movie_state)
+				.forEach(key => (movie_state[key] = obj[key]));
+
+			return movie_state;
+		}
+
+		componentWillReceiveProps(nextProps) {
+			const next_id = nextProps.match.params.movie_id;
+			if (this.getMovieId() !== next_id) {
+				this.getMovie(next_id);
+				// setTimeout(() => this.getMovie(), 0); //next tick
+			}
+		}
+
+		componentDidMount() {
+			this.getMovie(this.getMovieId());
+		}
+
+		shouldComponentUpdate(nextProps, nextState) {
+			return (
+				nextState.loaded !== this.state.loaded ||
+				nextProps.match.params.movie_id !== this.getMovieId()
+			);
+		}
+
+		render() {
+			return (
+				<div>
+					MoviesDetailsWrapper
+					<MovieDisplayComponent {...this.state} />
+				</div>
+			);
+		}
+	};
 }
-export default MovieDetailsWrapper;
