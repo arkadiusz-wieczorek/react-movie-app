12d6219f6198b9c4f03c7c8aabe200ce9da3c949
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2017-05-09 00:19:44 +0200

    Add not found page and NotFoundPage component

---
+++ lib/components/not-found-page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class NotFoundPage extends React.Component {
+	render() {
+		return (
+			<div className="not-found">
+				Page not found.
+				Something went wrong!
+				<div className="image" />
+			</div>
+		);
+	}
+}
+
+export default NotFoundPage;
